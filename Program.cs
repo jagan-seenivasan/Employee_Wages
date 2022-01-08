49b47084b78f4b6b4413287166ad97efe8526788
jagan-seenivasan <85672216+jagan-seenivasan@users.noreply.github.com>
2022-01-08 22:49:28 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Employee_Wage
+{
+    class Program
+    {
+          public static void Main(string[] args)
+        {
+            UC1_EmpCheck.CheckEmployee();
+          UC2_DailyWage.CheckDailySalary();
+          UC3_PartTimeEmpAndWage.CheckPartTimeEmpWage();
+         UC4_PartTimeEmpWage_usingSwitch.CheckEmpWageUsingSwitch();
+         UC5_CalculateEmpSalaryForMonth.CheckFor20DaysInMonth();
+          UC6_SalaryForTotalWorkingHrs100_OrMonthsOf20.CheckUC6_SalaryForTotalWorkingHrs100_OrMonthsOf20();
+        }
+    }
+}
